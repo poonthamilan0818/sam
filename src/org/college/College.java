6329451e2bc515649298a40b694e1d3f98ac2a1b
syedfarith31 <syedfarith31@gmail.com>
2019-04-02 23:59:16 +0530

    qwert

---
+++ src/org/college/College.java
@@ -0,0 +1,24 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("College name:Aksheyaa college of engineering");
+	}
+	public void collegeCode() {
+		System.out.println("College code:4205");
+	}
+	public void collegeRank() {
+		System.out.println("College rank:012");
+	}
+	public static void main(String[] args) {
+		College clg=new College();
+		clg.collegeName();clg.collegeCode();clg.collegeRank();
+		Student std=new Student();
+		std.studentName();std.studentId();std.studentDept();
+		Hostel hs=new Hostel();
+		hs.hostelName();
+		Dept dp=new Dept();
+		dp.deptName();
+	}
+
+}
---
+++ src/org/college/Student.java
@@ -0,0 +1,13 @@
+package org.college;
+
+public class Student {
+	public void studentName() {
+		System.out.println("Student name: JONAM");
+	}
+	public void studentDept() {
+		System.out.println("Student Dept:MECH");
+	}
+	public void studentId() {
+		System.out.println("StudentID:012345");
+	}
+}
